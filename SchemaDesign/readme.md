9b7ee4c383696ec56e179757e82eca17ef4860ca
Arnab Das <abc@xyz.com>
2023-12-07 09:01:28 +0530

    Initial Check in

---
+++ SchemaDesign/readme.md
@@ -0,0 +1,11 @@
+
+# Schema Design
+
+### Table: **User**
+
+| Column Name  | Data Type | Primary Key | Foreign Key |
+| ------------- | ------------- |
+| id  | int  | Yes | |
+
+| created_at  | DateTime  |  | |
+| last_modified  | DateTime  |  | |
---
+++ models/models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from typing import List
+
+
+class BaseModel:
+    id:int
+    created_at:datetime
+    last_modified:datetime
+
+
+class User(BaseModel):
+    name:str
+    phone_number:str
+    password:str
+
+class Group(BaseModel):
+    name:str
+    user_list: List[User]
+    created_by:User
+
+
+class Expense(BaseModel):
+    paid_by:User
+    paid_amount:float
+    group:Group
+    is_group_expense:bool
+
+
+class ExpenseSplit(BaseModel):
+    direct_expense: Expense
+    user_split_for:User
+    user_split_amount:float 
+
+
+class Transaction(BaseModel):
+    is_group_transaction:bool
+    group:Group
+    received_by:User
+    paid_by:User
+    amount:float
+    is_settled:bool
+
+
+
+
